84c9228310ece8cb4e067060adbd76facf8cec7e
Stephen Basile <stephen@snbasile.com>
2020-01-21 13:03:11 -0500

    missing end tag

--- lib/fastlyctl/utils.rb
+++ lib/fastlyctl/utils.rb
@@ -76,6 +76,7 @@ module FastlyCTL
     def self.percent_encode(string)
       # CGI.escape replace whitespace to "+" which is "%20" in a percent-encoding manner
       CGI.escape(string).gsub('+', '%20')
+    end
 
     def self.filter_vnd(haystack,needle)
       results = []
