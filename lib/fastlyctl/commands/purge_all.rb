c801e99745f0529d6d1048ac757e93d321405ad2
Stephen Basile <stephen@snbasile.com>
2020-01-21 09:44:45 -0500

    fix --s on purge_all

--- lib/fastlyctl/commands/purge_all.rb
+++ lib/fastlyctl/commands/purge_all.rb
@@ -3,11 +3,10 @@ module FastlyCTL
     desc "purge_all", "Purge all content from a service."
     method_option :service, :aliases => ["--s"]
     def purge_all
-      parsed_id = FastlyCTL::Utils.parse_directory
+      id = FastlyCTL::Utils.parse_directory unless options[:service]
+      id ||= options[:service]
 
-      id = FastlyCTL::Utils.parse_directory
-
-      abort "Could not parse service id from directory. Use --s <service> to specify, vcl download, then try again." unless (id || options[:service])
+      abort "Could not parse service id from directory. Use --s <service> to specify, vcl download, then try again." unless id
 
       FastlyCTL::Fetcher.api_request(:post, "/service/#{id}/purge_all")
 
